64b4b6498f5b24195d20fa470bb3f63d52a9ea25
AzureAD\AndreaSpiteri <andrea.spiteri@mo-sys.com>
2025-02-11 12:29:26 +0000

    Updated copyright data.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@REM Copyright 2024 Mo-Sys Engineering Ltd
+@REM Copyright 2025 Mo-Sys Engineering Ltd
 @REM Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), 
 @REM to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, 
 @REM distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
