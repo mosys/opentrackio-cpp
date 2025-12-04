6edf04e2c2709ca55c15a6f1969632b6337a1fbf
Kyle Norton <kyle.norton1@Live.com>
2025-12-05 09:21:24 +1100

    Add missing field - pinholefocallength

--- src/OpenTrackIOSample.cpp
+++ src/OpenTrackIOSample.cpp
@@ -230,6 +230,7 @@ namespace opentrackio
         }
 
         assignJson(baseJson["lens"], "fStop", lens->fStop);
+        assignJson(baseJson["lens"], "pinholeFocalLength", lens->pinholeFocalLength);
         assignJson(baseJson["lens"], "focusDistance", lens->focusDistance);
 
         if (lens->projectionOffset.has_value())
